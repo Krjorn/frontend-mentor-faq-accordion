fa403f8b73c11fc9151a3eca4c59aa4e2235dff8
Krjorn <christopher990172@gmail.com>
2024-09-16 03:41:41 -0300

    Finish style and functionality in faq-topic component

--- src/app/components/faq-topic/faq-topic.component.ts
+++ src/app/components/faq-topic/faq-topic.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-faq-topic',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule ],
   templateUrl: './faq-topic.component.html',
   styleUrl: './faq-topic.component.css'
 })
 export class FaqTopicComponent {
+  @Input()
+  title = '';
 
+  @Input()
+  text = '';
+
+  @Input()
+  isLast = false;
+  
+  isExpanded = false;
+  iconPlus = 'icon-plus';
+  iconMinus = 'icon-minus';
+
+  public expandTopic(): void {
+    this.isExpanded = !this.isExpanded;
+  }
 }
